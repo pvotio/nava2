1438a2fb2d0f87eb763c1a23bc4c57abf369a58b
Ali Moghimi <mghmiali@gmail.com>
2025-09-20 15:05:22 +0000

    fix: reference to correct db uri variable

--- app/db/postgres.py
+++ app/db/postgres.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import declarative_base, scoped_session, sessionmaker
 
 from ..core.config import settings
 
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True, future=True)
+engine = create_engine(settings.sqlalchemy_database_uri, pool_pre_ping=True, future=True)
 SessionLocal = scoped_session(
     sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
 )
